f0c59a3a9544e0824462f808ce606876646ece59
Александр <@192.168.0.100>
2021-06-06 19:45:23 +0800

    121212121

--- src/ModLoad.java
+++ src/ModLoad.java
@@ -1,5 +1,9 @@
+import java.util.Locale.Category;
+
 import content.ModBlocks;
+import mindustry.content.Items;
 import mindustry.mod.Mod;
+import mindustry.type.ItemStack;
 
 public class ModLoad extends Mod{
 	public ModLoad(){
--- src/content/ModBlocks.java
+++ src/content/ModBlocks.java
@@ -1,15 +1,21 @@
 package content;
 
+
 import blocks.Teleporter;
+import mindustry.content.Items;
 import mindustry.ctype.ContentList;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.world.Block;
 
 public class ModBlocks implements ContentList{
-
+	public static Block teleporter;
 	@Override
 	public void load() {
 		Teleporter teleporter = new Teleporter("teleporter") {{
 			health = 100;
 			update = true;
+			requirements(Category.distribution, ItemStack.with(Items.phaseFabric, 5, Items.silicon, 7, Items.lead, 10, Items.graphite, 10));
 		}};
 		
 	}
