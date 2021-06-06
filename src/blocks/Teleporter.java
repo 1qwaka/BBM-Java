0e46a04b7b5bf38605e248858cb5be1afc51b722
Александр <@192.168.0.100>
2021-06-06 20:17:03 +0800

    LOL

--- src/blocks/Teleporter.java
+++ src/blocks/Teleporter.java
@@ -2,9 +2,14 @@ package blocks;
 
 import java.util.Arrays;
 
+import arc.math.Mathf;
 import arc.struct.Seq;
 import arc.util.Log;
+import arc.util.Time;
+import mindustry.Vars;
 import mindustry.gen.Building;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Pal;
 import mindustry.world.Block;
 
 public class Teleporter extends Block{
@@ -16,7 +21,6 @@ public class Teleporter extends Block{
 		});
 		  };
 	public class TelepolterBuild extends Building{
-		public Integer link;
 		Seq<Integer> b = new Seq<Integer>();
 		public void addLink(int point){
 			if(b.contains(point)){
@@ -38,8 +42,17 @@ public class Teleporter extends Block{
 		}
 		@Override
 		public void update(){
-			Log.info(link);
 			Log.info(Arrays.toString(b.items));
 	}
+		@Override
+		public void drawConfigure(){
+			super.drawConfigure();
+		    float sin = Mathf.absin(Time.time, 6, 1);
+		    Drawf.circles(this.x, this.y, (this.block.size / 2 + 1) * Vars.tilesize + sin - 2);
+		    for(int i = 0; i>= b.size; i++) {
+		    	Building build =  Vars.world.build(b.get(i));
+		    	 Drawf.circles(build.x, build.y, (build.block.size / 2 + 1) * Vars.tilesize + sin - 2, Pal.place);
+		    }
+		}
 	}
 }
